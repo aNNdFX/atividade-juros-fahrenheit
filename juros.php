571173981f8e03dc9a79451e70f7534bf1586df8
aNNdFX <andre.xilef14@gmail.com>
2024-11-26 08:33:28 -0300

    Update juros.php

--- juros.php
+++ juros.php
@@ -2,7 +2,7 @@
 <html lang="br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta Name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cálculo de Juros Simples</title>
 </head>
 <body style="background-color: black; color: white;">
